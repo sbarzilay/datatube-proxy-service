a6cc20acec7a7441fa80b20a6bb59e18e5c179f9
Tal Sheldon <talshel@checkpoint.com>
2021-10-18 16:46:50 +0300

    logger to stdout

--- app.py
+++ app.py
@@ -14,8 +14,11 @@ app = Flask(__name__)
 client = Kusto()
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 application_insights_key = os.getenv('APPINSIGHTS_INSTRUMENTATIONKEY')
